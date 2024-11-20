3aaff86988db13075e6d3afb42d67cab7784d804
LynXHashib <hashib4305@gmail.com>
2024-11-21 01:54:05 +0600

    Initial commit

---
+++ hypr/scripts/wallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Path to the config.ini file
+CONFIG_FILE="/home/hashib/.config/waypaper/config.ini"
+
+wallpaper=$(grep -oP '(?<=^wallpaper = ).*' <(grep -A 50 "\[Settings\]" "$CONFIG_FILE"))
+
+# Expand the `~` in the path
+wallpaper=$(eval echo "$wallpaper")
+
+# Use the wallpaper value
+echo "Setting wallpaper to $wallpaper"
+killall waybar
+wal --backend colorz -i $wallpaper --cols16 
+waybar
+gi
