5a32f4891878cc266986a46724b8765a268bb03d
LynXHashib <hashib4305@gmail.com>
2024-11-25 20:50:35 +0600

    Add wallpaper

--- ags/app.ts
+++ ags/app.ts
@@ -1,10 +1,33 @@
-import { App } from "astal/gtk3"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
+#!/usr/bin/gjs -m
+import { register } from "astal/gobject"
+import { App, Astal } from "astal/gtk4"
+import Template from "./Bar.blp"
 
-App.start({
-    css: style,
-    main() {
-        App.get_monitors().map(Bar)
+const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+const calendar = Widget.Calendar({
+    showDayNames: true,
+    showDetails: true,
+    showHeading: true,
+    showWeekNumbers: true,
+    detail: (self, y, m, d) => {
+        return `<span color="white">${y}. ${m}. ${d}.</span>`
+    },
+    onDaySelected: ({ date: [y, m, d] }) => {
+        print(`${y}. ${m}. ${d}.`)
     },
 })
+@register({ GTypeName: "Bar", Template })
+class Bar extends Astal.Window {
+}
+
+App.start({
+    instanceName: "bar",
+    main() {
+        new Bar({
+            application: App,
+            anchor: TOP | LEFT | RIGHT,
+            visible: true,
+        })
+        calendar
+    }
+    })
