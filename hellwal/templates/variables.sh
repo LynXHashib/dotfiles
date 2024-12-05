c1e491595f5f586faaa669da0f513823c4ed0e4e
LynXHashib <hashib4305@gmail.com>
2024-12-05 21:24:04 +0600

    done

--- hellwal/templates/variables.sh
+++ hellwal/templates/variables.sh
@@ -8,8 +8,8 @@ cursor='%%cursor%%'
 border='%%border%%'
 
 color0='%%color0.hex%%'
-color1='%%color1.hex%%'
-color2='%%color2.hex%%'
+color1='%%color15.hex%%'
+color2='%%color14.hex%%'
 color3='%%color3.hex%%'
 color4='%%color4.hex%%'
 color5='%%color5.hex%%'
@@ -21,8 +21,8 @@ color10='%%color10.hex%%'
 color11='%%color11.hex%%'
 color12='%%color12.hex%%'
 color13='%%color13.hex%%'
-color14='%%color14.hex%%'
-color15='%%color15.hex%%'
+color14='%%color2.hex%%'
+color15='%%color1.hex%%'
 
 # FZF colors
 export FZF_DEFAULT_OPTS="
