f505812c13ae2060aa06d458d7f3601acda66ddc
LynXHashib <hashib4305@gmail.com>
2024-12-12 14:02:39 +0600

    ags bar

--- ags/app.ts
+++ ags/app.ts
@@ -1,33 +1,13 @@
-#!/usr/bin/gjs -m
-import { register } from "astal/gobject"
-import { App, Astal } from "astal/gtk4"
-import Template from "./Bar.blp"
-
-const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
-const calendar = Widget.Calendar({
-    showDayNames: true,
-    showDetails: true,
-    showHeading: true,
-    showWeekNumbers: true,
-    detail: (self, y, m, d) => {
-        return `<span color="white">${y}. ${m}. ${d}.</span>`
-    },
-    onDaySelected: ({ date: [y, m, d] }) => {
-        print(`${y}. ${m}. ${d}.`)
-    },
-})
-@register({ GTypeName: "Bar", Template })
-class Bar extends Astal.Window {
-}
+import { App } from "astal/gtk3"
+import style from "./style.scss"
+import Bar from "./widget/Bar"
 
 App.start({
-    instanceName: "bar",
-    main() {
-        new Bar({
-            application: App,
-            anchor: TOP | LEFT | RIGHT,
-            visible: true,
-        })
-        calendar
-    }
-    })
+    css: style,
+    instanceName: "js",
+    requestHandler(request, res) {
+        print(request)
+        res("ok")
+    },
+    main: () => App.get_monitors().map(Bar),
+})
